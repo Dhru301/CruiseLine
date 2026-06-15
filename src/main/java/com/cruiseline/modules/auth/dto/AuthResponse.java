31e0da3947d9cc1fc4ffefd058ed7bf920a87e98
Gopivivek17 <Shiramshetti.GopiVivek@cognizant.com>
2026-06-15 10:48:24 +0530

    AuthResponse dto

---
+++ src/main/java/com/cruiseline/modules/auth/dto/AuthResponse.java
@@ -0,0 +1,22 @@
+package com.cruiseline.modules.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+	
+	 	private String accessToken;
+	    private String refreshToken;
+	    private String tokenType;
+	    private long expiresInMs;
+	    private Long userId;
+	    private String name;
+	    private String email;
+	    private String role;
+}
